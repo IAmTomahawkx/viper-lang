e61da86f0e268229815ea747a0b50e58c7330660
Henry <14969260+henry232323@users.noreply.github.com>
2021-03-31 17:55:37 -0600

    Update objects.py

--- viper/objects.py
+++ viper/objects.py
@@ -93,10 +93,9 @@ class PyNativeObjectWrapper(VPObject):
         if not callable(item):
             raise errors.ViperExecutionError(runner, line, f"<PyObject_{item}> is not callable")
 
-        if inspect.iscoroutine(item) or inspect.iscoroutinefunction(item):
-            resp = await item(line, runner, *args)
-        else:
-            resp = item(line, runner, *args)
+        resp = item(line, runner, *args)
+        if inspect.isawaitable(resp):
+            resp = await resp
 
         if not isinstance(resp, VPObject):
             resp = PyObjectWrapper(runner, resp)
@@ -124,10 +123,9 @@ class PyObjectWrapper(PyNativeObjectWrapper):
             else:
                 _args.append(arg)
  
-        if inspect.iscoroutine(item) or inspect.iscoroutinefunction(item):
-            resp = await item(*_args)
-        else:
-            resp = item(*_args)
+        resp = item(*_args)
+        if inspect.isawaitable(resp):
+            resp = await resp
 
 
         if not isinstance(resp, VPObject):
